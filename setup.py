8b7f69e429aa787a4445ceb03e4446cd973a6f98
cliffyb824 <qycliffy@gmail.com>
2021-11-19 12:52:44 -0600

    Signed-off-by: cliffyb824 <qycliffy@gmail.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+from setuptools import setup
+
+setup(name='dsdn_distributions_yua',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsdn_distributions_yua'],
+      zip_safe=False)
